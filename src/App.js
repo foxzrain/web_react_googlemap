e871a5d1c117266c36af558f809b86cb335795f3
Pichaya Deachpol <pichaya@sic.co.th>
2023-08-15 17:32:52 +0700

    - search restaurant, bakery and cafe in 5 km around center location - A Places Nearby search is initiated with a call to the PlacesService's nearbySearch() method

--- src/App.js
+++ src/App.js
@@ -25,14 +25,11 @@ export default class App extends Component {
 
         // function load when create page
         loader.load().then((google) => {
-            const map = new google.maps.Map(
+          this.google = google;
+
+          const map = new google.maps.Map(
                 this.googleMapDiv,
                 defaultMapOptions);
-
-            const marker = new google.maps.Marker({
-              position: { lat: 13.779820829768585, lng: 100.54464812602707 },
-              map: map, // must have
-            });
             /*
                 store them in the state so you can use it later
                 E.g. call a function on the map object:
@@ -40,13 +37,45 @@ export default class App extends Component {
                 E.g. create a new marker:
                     new this.state.google.maps.Marker(...)
             */
+            const marker = new google.maps.Marker({
+              position: {
+                lat: 13.779820829768585,
+                lng: 100.54464812602707
+            },
+              map: map, // must have
+            });
+
+            // center location state
             this.setState({
                 google: google,
                 map: map,
                 marker: marker,
             });
+
+            // search restaurant, bakery and cafe in 5 km around center location
+            var request = {
+              location: {
+                lat: 13.779820829768585,
+                lng: 100.54464812602707
+            },
+              radius: '5000', // 5 km
+              type: ['restaurant', 'Bakery', 'Cafe']
+            };
+          
+            // A Places Nearby search is initiated with a call to the PlacesService's nearbySearch() method
+            const service = new google.maps.places.PlacesService(map);
+            service.nearbySearch(request, this.callback);
         });
-    }
+    };
+
+    callback(results, status) {
+      if (status === 'OK') {
+        for (var i = 0; i < results.length; i++) {
+          if (!results[i].geometry || !results[i].geometry.location) return;
+          console.log(results[i].name);
+        }
+      }
+    };
 
     render() {
         return (
