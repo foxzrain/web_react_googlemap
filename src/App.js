70da472566fa82fe32ba3743d2cde5e097e26f90
Pichaya Deachpol <pichaya@sic.co.th>
2023-08-15 13:46:23 +0700

    - Rendering Google Map inside React application - Integration of Google Maps with React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import { useMemo } from "react";
+import React from 'react';
+import "./App.css";
 
-function App() {
+const App = () => {
+
+  // useLoadScript: It loads the Google Maps API script.
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  });
+
+  // center: Sets a default center of the map
+  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isLoaded 
+      ? (
+        <h1>Loading...</h1>
+      ) 
+      : (
+        // GoogleMap: It is the main component inside which all the other map components render.
+        <GoogleMap
+          // mapContainerClassName: CSS class name that specifies the height and width of the GoogleMap component
+          mapContainerClassName="map-container"
+          center={center} 
+          zoom={10} // zoom: Sets the initial zoom level of the map
+        />
+      )}
     </div>
   );
-}
+};
 
 export default App;
